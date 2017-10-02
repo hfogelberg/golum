d7ffdaba04629f410c76da1facb861a73ff0fceb
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-10-02 10:10:33 +0100

    Test linear regression model

--- linearRegression_test.go
+++ linearRegression_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TrainModelTest(t *testing.T) {
+func TrainLinearModelTest(t *testing.T) {
 	file := "data/salary_data.csv"
 
 	trainCsv, _, err := TrainTestSplit(file, 0.3)
@@ -14,11 +14,28 @@ func TrainModelTest(t *testing.T) {
 		t.Error(err)
 	}
 
-	formula, err := TrainModel(trainCsv, 2, "Salary", "YearsExperience")
+	formula, err := TrainLinearModel(trainCsv, 2, "Salary", "YearsExperience")
 	if err != nil {
 		t.Error(err.Error())
 	}
 
+	fmt.Println(formula)
+
+	os.Remove("test.csv")
+	os.Remove("train.csv")
+}
+
+func TestLinearModelTest(t *testing.T) {
+	file := "data/salary_data.csv"
+	_, testCsv, err := TrainTestSplit(file, 0.3)
+	if err != nil {
+		t.Error(err)
+	}
+
+	formula, err := TestLinearModel(testCsv, 2, 0, 1)
+	if err != nil {
+		t.Error(err.Error())
+	}
 	fmt.Println(formula)
 
 	os.Remove("test.csv")
